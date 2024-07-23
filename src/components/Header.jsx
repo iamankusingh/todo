2187b8ff05627be12a4e967afcabfde0d573bcf8
iamankusingh <ankusingh@gmail.com>
2024-07-23 22:44:44 +0530

    some updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,6 @@ import stroke from "../assets/pink-stroke.png"
 const Header = () => {
   return (
     <header className="w-full p-4  text-center cursor-default bg-stroke">
-      {/* <img src={stroke} alt="stroke" className="absolute right-1/2 " /> */}
       <h1 className="text-4xl font-bold z-10">Todo Tasks</h1>
       <h3 className="text-xl z-10">Manage your all todos here...</h3>
     </header>
